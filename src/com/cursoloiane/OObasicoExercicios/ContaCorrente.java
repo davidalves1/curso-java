20b7042926d9d3405d0fbe3f18b785f388628025
davidalves1 <alves.david@outlook.com>
2019-11-03 18:56:04 -0200

    Criar novos métodos para a conta

--- src/com/cursoloiane/OObasicoExercicios/ContaCorrente.java
+++ src/com/cursoloiane/OObasicoExercicios/ContaCorrente.java
@@ -1,5 +1,65 @@
 package com.cursoloiane.OObasicoExercicios;
 
-public class ContaCorrente {
+import java.util.ArrayList;
 
+public class ContaCorrente {
+	String numero;
+	String agencia;
+	double saldo;
+	boolean especial;
+	double limiteEspecial;
+	ArrayList<String> extrato;
+	
+	public ContaCorrente(String numero, String agencia, double saldo, boolean especial, double limiteEspecial) {
+		super();
+		this.numero = numero;
+		this.agencia = agencia;
+		this.saldo = saldo;
+		this.especial = especial;
+		this.limiteEspecial = limiteEspecial;
+		this.extrato = new ArrayList<String>();
+	}
+
+	double consultarSaldoTotal() {
+		return this.saldo + this.limiteEspecial;
+	}
+	
+	void exibirSaldo() {
+		System.out.println("O saldo total atual é de: R$" + consultarSaldoTotal());
+	}
+	
+	void incrementarExtrato(String valor) {
+		extrato.add(valor);
+	}
+	
+	void exibirExtrato() {
+		System.out.println("\n### Extrato da conta " + numero + " ###\n--------------------");
+		int i = 1;
+		for (String item : extrato) {
+			System.out.println(i + ". " + item);
+			i++;
+		}
+		System.out.println("--------------------");
+		System.out.println("Saldo: R$" + consultarSaldoTotal());
+	}
+	
+	boolean realizarSaque(double valorSacar) {
+		if (saldo >= valorSacar) {
+			saldo -= valorSacar;
+			incrementarExtrato("D R$" + valorSacar);
+			return true;
+		} else if (especial && saldo + limiteEspecial >= valorSacar) {
+			limiteEspecial -= valorSacar - saldo;
+			saldo = 0;
+			incrementarExtrato("D R$" + valorSacar);
+			return true;
+		} else {	
+			return false;
+		}
+	}
+	
+	void realizarDeposito(double valorDepositado) {
+		incrementarExtrato("C R$" + valorDepositado);
+		saldo += valorDepositado;
+	}
 }
--- src/com/cursoloiane/OObasicoExercicios/Lista1.java
+++ src/com/cursoloiane/OObasicoExercicios/Lista1.java
@@ -6,13 +6,39 @@ public class Lista1 {
 		// TODO Auto-generated method stub
 		// Exercício 1:
 		// Criar métodos para ligar e desligar a lâmpada
-		Lampada lampada = new Lampada();
+		// Lampada lampada = new Lampada();
 		
-		lampada.mostrarEstado();
+		/*lampada.mostrarEstado();
 		lampada.ligar();
 		lampada.mostrarEstado();
 		lampada.desligar();
-		lampada.mostrarEstado();
+		lampada.mostrarEstado();*/
+		
+		// Exercício 2
+		// Criar uma classe que representa uma conta corrente com número, saldo, limite e se é especial ou não.
+		// Nesta classe também deve ter os métodos para visualizar o saldo, realizar saque e depósito, verificar
+		// se está usando cheque especial ou não
+		ContaCorrente conta = new ContaCorrente("1122", "0212", 100, true, 100);
+		
+		conta.exibirSaldo();
+		
+		boolean saque = conta.realizarSaque(120);
+		
+		if (saque) {
+			System.out.println("Saque realizado com sucesso!");
+		} else {
+			System.out.println("Não foi possível realizar saque");
+		}
+		
+		conta.realizarDeposito(100);
+		conta.realizarDeposito(200);
+		conta.realizarDeposito(100);
+		conta.realizarDeposito(50);
+		conta.realizarSaque(300);
+		conta.realizarDeposito(200);
+		conta.realizarSaque(400);
+		
+		conta.exibirExtrato();
 	}
 
 }
